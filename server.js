14aa7cc6d055104794b8949ce82a19bcf760f799
izu-stanley <izuchukwuuchegbu@gmail.com>
2021-06-01 18:16:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 
 app.get('/', (req, res) => {    
-   res.status(200).send("Hello from app engine!! Deployed from a branch's pr");
+   res.status(200).send("Hello from app engine!! New deployement");
 });
 
 app.listen(process.env.PORT);
